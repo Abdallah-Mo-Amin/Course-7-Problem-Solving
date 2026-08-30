fa4a2a82413cbe3e5838e9bdd1fe2ca124b6ce48
Abdallah-Mo-Amin <abdallahamin567@gmail.com>
2026-08-30 13:28:48 +0300

    Create 03-sum each row in matrix in array.cpp

---
+++ 03-sum each row in matrix in array.cpp
@@ -0,0 +1,88 @@
+#pragma warning(disable : 4996)
+
+#include <iostream>
+#include <cstdlib>  
+#include <iomanip> // this library stored the std::setw
+#include <cmath>
+#include <string> // this library stired the string object
+#include <vector>
+#include <cctype>
+#include <ctime>
+#include <fstream> 
+using namespace std;
+
+int RandomNumber(int From, int To)
+{
+    int randNum = rand() % (To - From + 1) + From;
+    return randNum;
+}
+
+void FillMatrixWithRandomNumbers(int arr[3][3], short Rows, short Cols)
+{
+    for (short i = 0; i < Rows; i++)
+    {
+        for (short j = 0; j < Cols; j++)
+        {
+            arr[i][j] = RandomNumber(1, 100);
+        }
+    }
+}
+
+void PrintMatrix(int Matrix[3][3], short Rows, short Cols)
+{
+    for (short i = 0; i < Rows; i++)
+    {
+        for (short j = 0; j < Cols; j++)
+        {
+            cout << setw(3) << Matrix[i][j] << "    ";
+        }
+        cout << endl;
+    }
+}
+
+int RowSum(int arr[3][3], short RowNumber, short Cols)
+{
+    int Sum = 0;
+
+    for (short j = 0; j <= Cols - 1; j++)
+    {
+        Sum += arr[RowNumber][j];
+    }
+    return Sum;
+}
+
+void SumMatrixRowsInArray(int arr[3][3], int arrSum[3], short Rows, short Cols)
+{
+    for (short i = 0; i < Rows; i++)
+    {
+        arrSum[i] = RowSum(arr, i, Cols);
+    }
+}
+
+void PrintRowsSumArray(int arr[3], short Rows)
+{
+    cout << "\nThe following are the sum of each row in the matrix:\n";
+    for (short i = 0; i < Rows; i++)
+    {
+        cout << "Row " << i + 1 << " Sum = " << arr[i] << endl;
+    }
+}
+
+int main() 
+{
+    srand((unsigned)time(NULL));
+
+    int arr[3][3];
+    int arrSum[3];
+    FillMatrixWithRandomNumbers(arr, 3, 3);
+
+    cout << "The following is a 3x3 random matrix:\n";
+    PrintMatrix(arr, 3, 3);
+
+    SumMatrixRowsInArray(arr, arrSum, 3, 3);
+    PrintRowsSumArray(arrSum, 3);
+
+    system("pause>0");
+
+    return 0;
+}
