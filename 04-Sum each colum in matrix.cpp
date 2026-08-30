00d0e03be6eab3257f669122bfd35900b127d56b
Abdallah-Mo-Amin <abdallahamin567@gmail.com>
2026-08-30 13:30:25 +0300

    Create 04-Sum each colum in matrix.cpp

---
+++ 04-Sum each colum in matrix.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <cstdlib>  
+#include <iomanip> // this library stored the std::setw
+#include <cmath>
+#include <string> // this library stired the string object
+#include <vector>
+#include <cctype>
+#include <ctime>
+#include <fstream> 
+using namespace std;
+
+int RandomNumber(int From, int To)
+{
+    int randNum = rand() % (To - From + 1) + From;
+    return randNum;
+}
+
+void FillMatrixWithRandomNumbers(int arr[3][3], short Rows, short Cols)
+{
+    for (short i = 0; i < Rows; i++)
+    {
+        for (short j = 0; j < Cols; j++)
+        {
+            arr[i][j] = RandomNumber(1, 100);
+        }
+    }
+}
+
+void PrintMatrix(int Matrix[3][3], short Rows, short Cols)
+{
+    for (short i = 0; i < Rows; i++)
+    {
+        for (short j = 0; j < Cols; j++)
+        {
+            cout << setw(3) << Matrix[i][j] << "    ";
+        }
+        cout << endl;
+    }
+}
+
+int ColSum(int arr[3][3], short Rows, short ColNumber)
+{
+    int Sum = 0; 
+
+    for (short i = 0; i <= Rows - 1; i++)
+    {
+        Sum += arr[i][ColNumber];
+    }
+    return Sum;
+}
+
+void PrintEachColSum(int arr[3][3], short Rows, short Cols)
+{
+    for (short j = 0; j <  Cols; j++)
+    {
+        cout << "col " << j + 1 << " Sum = " << ColSum(arr, Rows, j) << endl;
+    }
+}
+
+int main() 
+{
+    srand((unsigned)time(NULL));
+
+    int arr[3][3];
+    int arrSum[3];
+    FillMatrixWithRandomNumbers(arr, 3, 3);
+
+    cout << "The following is a 3x3 random matrix:\n";
+    PrintMatrix(arr, 3, 3);
+
+    PrintEachColSum(arr, 3, 3);
+
+    system("pause>0");
+
+    return 0;
+}
