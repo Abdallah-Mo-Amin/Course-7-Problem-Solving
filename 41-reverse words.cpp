e2fbc23b8ffd355aa856b6105a155e90c74a78b6
Abdallah-Mo-Amin <abdallahamin567@gmail.com>
2026-08-30 14:12:01 +0300

    Create 41-reverse words.cpp

---
+++ 41-reverse words.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <cstdlib>  
+#include <iomanip> // this library stored the std::setw
+#include <cmath>
+#include <string> // this library stired the string object
+#include <vector>
+#include <cctype>
+#include <ctime>
+#include <fstream> 
+#include "MyinputLib.h"
+
+using namespace std;
+using namespace MyInputLib;
+
+string ReadString()
+{
+    string S1 = "";
+    cout << "Please enter your string?\n";
+    getline(cin, S1);
+
+    return S1;
+}
+
+vector<string> SplitString(string S1, string delim)
+{
+    vector <string> vString;
+    short pos = 0;
+    string sWord;
+
+    while ((pos = S1.find(delim)) != std::string::npos)
+    {
+        sWord = S1.substr(0, pos);
+        if (sWord != "")
+        {
+            vString.push_back(sWord);
+        }
+        S1.erase(0, pos + delim.length());
+    }
+
+    if (S1 != "")
+    {
+        vString.push_back(S1);
+    }
+
+    return vString;
+}
+
+string ReverseWordsInString(string S1)
+{
+    vector<string> vString;
+    string S2 = "";
+
+    vString = SplitString(S1, " ");
+
+    // declare iterator
+    vector<string>::iterator iter = vString.end();
+
+    while (iter != vString.begin())
+    {
+        --iter;
+
+        S2 += *iter + " ";
+    }
+    S2 = S2.substr(0, S2.length() - 1); // remove last space
+    return S2;
+}
+
+int main()
+{
+    string S1 = ReadString();
+
+    cout << "\n\nString after reversing words:\n";
+    cout << ReverseWordsInString(S1);
+
+    system("pause>0");
+
+
+    return 0;
+}
